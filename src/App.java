38f5f733de255ce7108cb79decb07a05899cb9d2
Eduardo Navarro <eduardonavarro.test@gmail.com>
2025-02-03 23:04:10 -0500

    Cambios

--- src/App.java
+++ src/App.java
@@ -1,10 +1,24 @@
 import clases.Veterinaria;
+import clases.models.Caballo;
+import clases.models.Gato;
+import clases.models.Perro;
+import clases.models.Raza;
+import clases.models.TipoDeCaballo;
 
 public class App {
     public static void main(String[] args) {
         Veterinaria veterinaria = new Veterinaria("Veterinaria", "Calle 123", "123456789");
 
-        System.out.println(veterinaria.toString());
+        Perro miPerro = new Perro("Fiufiu", 3, 10.5, Raza.CALLEJERO);
+        Gato miGato = new Gato("Miau", 2, 4.5, Raza.CALLEJERO);
+        Caballo miCaballo = new Caballo("Caballo", 5, 100, TipoDeCaballo.DEPORTIVO);
+
+        System.out.println(miPerro.getNombre());
+        System.out.println("Edad: " + miGato.getEdad());
+        System.out.println(miCaballo.getTipoDeCaballo());
     }
     
 }
+
+
+/* Voy por el minuto 40 del video */
